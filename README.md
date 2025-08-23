3f90b2f5d7be078ed414def59930b67befd0b0ec
nahinbin <nahinbinmunir@gmail.com>
2025-08-23 09:39:24 +0800

    added login, register and logout function

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-#StudyBox is a study management system that let's a user to:
+StudyBox is a study management system that let's a user to:
 
 - **Plan Study:** Organize your study sessions and set goals.
 - **Calculate Grade:** Input your scores to automatically calculate your grades.
 - **Store Class Schedules:** Keep track of your classes and their timings.
 - **Track Attendance:** Monitor your attendance for each class.
+
+[![View Live](https://img.shields.io/badge/View-Live-brightgreen?style=for-the-badge&logo=render)](https://studybox.onrender.com/)
--- app.py
+++ app.py
@@ -1,14 +1,104 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, DataRequired, Length, Email, EqualTo, ValidationError
+from flask_bcrypt import Bcrypt, check_password_hash
+import os
+
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'Xapp0099'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+
+class Registerform(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
+    email = StringField(validators=[InputRequired(), Email()])
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("Username already exists")
+        
+    def validate_email(self, email):
+        existing_user_email = User.query.filter_by(email=email.data).first()
+        if existing_user_email:
+            raise ValidationError("Email already exists")
+
+class Loginform(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
+
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = Loginform()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = Registerform()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/task')
+def task():
+    return render_template('task.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    print(e.code)
+    print(e.description)
+    return "sorry, the page you are looking for does not exist :(", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
