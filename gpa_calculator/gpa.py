4525d60cb531d6c8d5bf78e0395c674c185c2e54
Bahaeldin Hytham <bahaeldinhytham@gmail.com>
2025-09-14 11:02:20 +0800

    created gpa calc

--- gpa_calculator/gpa.py
+++ gpa_calculator/gpa.py
@@ -14,25 +14,41 @@ db = SQLAlchemy(app)
 class Subjects(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), nullable = False)
-    gpa = db.Column(db.Integer, default = 0)
-    credits = db.Column(db.Integer, default = 0)
+    gpa = db.Column(db.Float, default = 0)
+    credits = db.Column(db.Integer)
 
-
+# function to calculate gpa
+def calc_gpa():
+    subjects = Subjects.query.all()
+    total_marks = 0
+    total_credits = 0
+    for subject in subjects:
+        mark = subject.gpa * subject.credits
+        total_marks += mark
+        total_credits += subject.credits
+    if total_credits == 0:
+        return 0
+    gpa = round(total_marks/total_credits, 2)
+    
+    return gpa if gpa else 0
 
 #homepage
 @app.route("/", methods=["GET", "POST"])
 def calc_home():
     if request.method == "GET":
         subject_list = Subjects.query.all()
-        return render_template("gpa.html", subject = subject_list)
+        current_gpa = calc_gpa()
+        return render_template("gpa.html", subjects = subject_list, gpa = current_gpa)
     elif request.method == "POST":
-        if "subject_name" in request.form:
-            new_subject = request.form.get('subject_name')
-            subject = Subjects(name = new_subject)
-            db.session.add(subject)
-            db.session.commit()
+        new_subject = request.form.get('subject_name')
+        subject_gpa = float(request.form.get('subject_gpa'))
+        subject_credits = int(request.form.get('subject_credits'))
+        subject = Subjects(name = new_subject, gpa = subject_gpa, credits = subject_credits)
+        db.session.add(subject)
+        db.session.commit()
         return redirect (url_for('calc_home'))
             
+
 
 if __name__ == "__main__":
     with app.app_context():
