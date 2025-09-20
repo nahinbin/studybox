1b6fb94a136d120b03ad06ab0808af25d0e13abd
Bahaeldin Hytham <bahaeldinhytham@gmail.com>
2025-09-20 16:50:52 +0800

    transfered all subject info inside dictionaries

---
+++ subject_enrollment/subject.py
@@ -0,0 +1,137 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///subject_enr.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+semester_dic = {
+    'sem_one' : ['GNB1114', 'CCT1114', 'LCE1113', 'CMT1114', 'CSP1114'],
+    'sem_two' : ['LCT1113', 'CDS1114', 'LEE1113', 'CMF1114','CMT1124', 'CPP1113'],
+    'sem_three' : ['LAE1113', 'CMT1134', 'CSP1123',]
+}
+
+subjects_info = {
+    # first sem
+    'GNB1114': {
+        'name': 'Introduction to Business Management',
+        'credit_hours': 4,
+        'assessments': {}
+    },
+
+    'CCT1114': {
+        'name': 'Introduction to Computing Technologies',
+        'credit_hours': 4,
+        'assessments': {
+            'Project': '30%',
+            'Role play': '20%'
+        }
+    },
+
+    'LCE1113': {
+        'name': 'Communicative English',
+        'credit_hours': 3,
+        'assessments': {}
+    },
+
+    'CMT1114': {
+        'name': 'Mathematics I',
+        'credit_hours': 4,
+        'assessments': {
+            'Quiz': '20%',
+            'Test': '30%'
+        }
+    },
+
+    'CSP1114': {
+        'name': 'Problem Solving & Program Design',
+        'credit_hours': 4,
+        'assessments': {
+            'Quiz': '20%',
+            'Test': '30%'
+        }
+    },
+
+    # second sem
+    'LCT1113': {
+        'name': 'Critical Thinking',
+        'credit_hours': 3,
+        'assessments': {}
+    },
+
+    'CDS1114': {
+        'name': 'Introduction to Digital Systems',
+        'credit_hours': 4,
+        'assessments': {
+            'Quiz': '10%',
+            'Test': '20%',
+            'Assignment': '20%'
+        }
+    },
+
+    'LEE1113': {
+        'name': 'Essential English',
+        'credit_hours': 3,
+        'assessments': {}
+    },
+
+    'CMF1114': {
+        'name': 'Multimedia Fundamental',
+        'credit_hours': 4,
+        'assessments': {
+            'Test': '20%',
+            'Lab Assessment': '30%'
+        }
+    },
+
+    'CMT1124': {
+        'name': 'Mathematics II',
+        'credit_hours': 4,
+        'assessments': {
+            'Quiz': '20%',
+            'Test': '30%'
+        }
+    },
+
+    'CPP1113': {
+        'name': 'Principles of Physics',
+        'credit_hours': 3,
+        'assessments': {
+            'Test': '30%',
+            'Quiz': '20%'
+        }
+    },
+
+    # third se
+    'LAE1113': {
+        'name': 'Academic English',
+        'credit_hours': 3,
+        'assessments': {}
+    },
+
+    'CMT1134': {
+        'name': 'Mathematics III',
+        'credit_hours': 4,
+        'assessments': {
+            'Quiz': '20%',
+            'Test': '30%'
+        }
+    },
+
+    'CSP1123': {
+        'name': 'Mini IT Project',
+        'credit_hours': 3,
+        'assessments': {
+            'Technical Report': '40%'
+        }
+    }
+}
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
